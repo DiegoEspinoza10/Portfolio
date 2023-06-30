1229e4f5443a8c7a8764db5ffeb79028c80e3253
diegoespinoza10 <diego.es10@icloud.com>
2023-06-29 19:23:54 -0600

    Adding the changes so the page is deployed with github.io

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
 import React from 'react';
-import {Helmet} from "react-helmet";
-import { BrowserRouter as Router, Route, BrowserRouter,Routes } from 'react-router-dom';
+import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Pages/Home';
+
 function App() {
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Diego Espinoza portfolio</title>
-        <meta name="description" content="Diego Espinoza portfolio" />
-        <meta name="keywords" content="Diego Espinoza portfolio" />
+        <title>Diego Espinoza Portfolio</title>
+        <meta name="description" content="Diego Espinoza Portfolio" />
+        <meta name="keywords" content="Diego Espinoza Portfolio" />
       </Helmet>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact Component={Home}/>
-      </Routes>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/Portfolio" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
+
